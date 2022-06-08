b83f1bd74b34561d81b4070146c16ba16163698a
z.s.d <zsd@umich.edu>
2022-06-08 11:47:15 -0400

    add Java module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module net.xyzsd.dichotomy {
+    requires static org.jetbrains.annotations;
+
+    exports net.xyzsd.dichotomy.either;
+    exports net.xyzsd.dichotomy.result;
+    exports net.xyzsd.dichotomy.function;
+    exports net.xyzsd.dichotomy;
+}
