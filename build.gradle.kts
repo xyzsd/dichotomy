8ee2fc0f338757eea4375eebc7d24aa47ee1ea4a
xyzsd <xyzsd@xyzsd.net>
2022-09-13 13:14:35 -0400

    Update build.gradle.kts
    initial publish config. 
      TODO: repository config (GH, OSSRH), credentials config
      TODO: signing !

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java-library")
+    id("maven-publish")
 }
 
 group = "net.xyzsd"
@@ -19,6 +20,36 @@ tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("GitHubPackages") {
+            name.set("dichotomy")
+            description.set("Either and Result monads for Java")
+            url.set("https://maven.pkg.github.com/xyzsd/dichotomy")   
+                        
+            licenses {
+                license {
+                    name.set("The Apache License, Version 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                }
+                
+                license {
+                    name.set("The MIT License")
+                    url.set("http://opensource.org/licenses/MIT")
+                }            
+            }
+            
+            developers {
+                developer {
+                    id.set("xyzsd")
+                    email.set("xyzsd@xyzsd.net")
+                }
+            }
+            
+            from(components["java"])
+        }
+    }
+}
 
 // for reproducible builds
 tasks.withType<AbstractArchiveTask>().configureEach {
