e44557e86e55c6b7c3c33ae43e9ee15fc8ec85df
xyzsd <xyzsd@xyzsd.net>
2022-09-15 13:27:49 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,8 +83,8 @@ publishing {
         maven {
             name = "OSSRH"
             
-            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
+            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
             url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
         
             credentials {
