54097a1ac0edbeaeacec465bf525a9ee104a8f31
z.s.d. <xyzsd@xyzsd.net>
2023-12-18 02:14:37 -0500

    build update (JUnit) for future gradle versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     compileOnly("org.jetbrains:annotations:24.0.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 java { 
