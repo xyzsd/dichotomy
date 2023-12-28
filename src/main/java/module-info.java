5fa5047cdaf4e7c171723477430fb0d4a9fd26a6
z.s.d. <xyzsd@xyzsd.net>
2023-12-28 16:35:18 -0500

    Minor module-info.java updates

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,20 +1,19 @@
 /**
  * Module declaration for the net.xyzsd.dichotomy module.
- * This module exports three packages:
+ * This module exports the following packages:
  * <ul>
- * <li>net.xyzsd.dichotomy.trying.function</li>
  * <li>net.xyzsd.dichotomy</li>
+ * <li>net.xyzsd.dichotomy.collectors</li>
  * <li>net.xyzsd.dichotomy.trying</li>
+ * <li>net.xyzsd.dichotomy.trying.function</li>
  * </ul>
- *
- * This module requires the org.jetbrains.annotations module in a static manner.
  *
  */
 module net.xyzsd.dichotomy {
     requires static org.jetbrains.annotations;
 
-    exports net.xyzsd.dichotomy.trying.function;
     exports net.xyzsd.dichotomy;
-    exports net.xyzsd.dichotomy.trying;
     exports net.xyzsd.dichotomy.collectors;
+    exports net.xyzsd.dichotomy.trying;
+    exports net.xyzsd.dichotomy.trying.function;
 }
